07d07ff72cd88f4d57358ed8abdd40ced1b87d3c
ZoeyOneOhOne <cole.perry113@gmail.com>
2023-10-17 01:50:11 -0400

    Fixed some things in ping

--- commands/pings.js
+++ commands/pings.js
@@ -1,13 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
-const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong, hopefully!'),
 	async execute(interaction) {
 		try {
-			const message = await interaction.reply({ content: 'Pong!', fetchReply: true });
+			await interaction.reply('Pong!');
 		} catch (error) {
 			console.error('An error occurred:', error);
 			await interaction.reply('An error occurred while processing your command.');
